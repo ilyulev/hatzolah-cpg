9f6a616c34413287f95132a998912bed702ccba4
Igor Lyulev <syberex@gmail.com>
2026-08-13 17:02:14 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 //   everything else           -> stale-while-revalidate (instant, self-healing)
 // Offline still works: anything already cached is served when the network fails,
 // which is the whole point of this app in the field.
-const VERSION = '202608130519-aa7f801';
+const VERSION = '202608130702-ab04a05';
 const CACHE_NAME = `hatzolah-cpg-${VERSION}`;
 const OFFLINE_URL = '/hatzolah-cpg/index.html';
 
